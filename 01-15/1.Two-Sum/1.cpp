c86dceee52eae589943fb80904f725273b142de6
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-11-18 12:12:26 +0900

    add parent folder

---
+++ 01-15/1.Two-Sum/1.cpp
@@ -0,0 +1,29 @@
+#if __has_include("../debug.hpp")
+#include "../debug.hpp"
+#endif
+// ここまでローカルでのデバッグ用なので気にしないでください --------------------
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> order(nums.size());
+    iota(order.begin(), order.end(), 0);
+    sort(order.begin(), order.end(), [&](auto a, auto b) {
+      return nums[a] < nums[b];
+    });
+    int right_i = nums.size() - 1;
+    for (int left_i = 0; left_i < nums.size(); ++left_i) {
+      auto left = nums[order[left_i]];
+      while (right_i >= 0 && nums[order[right_i]] > target - left) --right_i;
+      if (nums[order[right_i]] == target - left && left_i != right_i) {
+        return {order[left_i], order[right_i]};
+      }
+    }
+
+    return {};  // to avoid compile errors
+  }
+};

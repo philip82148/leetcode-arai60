ca070792b34f5a3c2bbe3a4e36a728289878bd5f
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-11-20 23:35:23 +0900

    improve code

--- 01-15/3.Longest-Substring-Without-Repeating-Characters/1.cpp
+++ 01-15/3.Longest-Substring-Without-Repeating-Characters/1.cpp
@@ -10,17 +10,19 @@ using namespace std;
 class Solution {
  public:
   int lengthOfLongestSubstring(string s) {
-    vector<int> ascii_to_last_index(256, -1);
-    int max_len = 0, unique_string_begin = 0;
+    vector<int> char_code_to_last_index(256, -1);
+    int max_len = 0, unique_str_begin = 0;
 
     for (int i = 0; i < s.size(); ++i) {
-      if (ascii_to_last_index[s[i]] >= unique_string_begin) {
-        unique_string_begin = ascii_to_last_index[s[i]] + 1;
+      unsigned char char_code = s[i];
+
+      if (char_code_to_last_index[char_code] >= unique_str_begin) {
+        unique_str_begin = char_code_to_last_index[char_code] + 1;
       }
 
-      // [unique_string_begin, i]が重複なしの範囲
-      max_len = max(max_len, i - unique_string_begin + 1);
-      ascii_to_last_index[s[i]] = i;
+      // [unique_str_begin, i]が重複なしの範囲
+      max_len = max(max_len, i - unique_str_begin + 1);
+      char_code_to_last_index[char_code] = i;
     }
     return max_len;
   }
